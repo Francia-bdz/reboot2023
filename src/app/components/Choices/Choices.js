6520c1a13b8e66930e6f82761842e751e38c32fe
ccadran <clariocadran33@gmail.com>
2023-11-15 17:53:54 +0100

    architecture 2

--- src/app/components/Choices/Choices.js
+++ src/app/components/Choices/Choices.js
@@ -1,10 +1,16 @@
 import Choice from "../Choice/Choice";
 import styles from "./style.module.scss";
-export default function Choices({ choices }) {
+export default function Choices({ choices, onChoiceClick }) {
   return (
     <div>
       {choices.map((choice, index) => {
-        return <Choice choice={choice} key={index} />;
+        return (
+          <Choice
+            choice={choice}
+            key={index}
+            onClick={() => onChoiceClick(choice.response)}
+          />
+        );
       })}
     </div>
   );
