ca6fd42ce4830132ec2f57c5f0b68b75c10350e0
ccadran <clariocadran33@gmail.com>
2023-11-16 09:25:46 +0100

    remove comment for deployment

--- src/app/components/Scene/Scene.js
+++ src/app/components/Scene/Scene.js
@@ -2,7 +2,7 @@ import styles from "./scene.module.scss";
 export default function Scene({ scene, response }) {
   return (
     <div className={styles.sceneContainer}>
-      <div className={styles.character}>//image du perso</div>
+      <div className={styles.character}></div>
       <div className={styles.bubble}>
         {response ? <h1>{response}</h1> : <h1>{scene.question}</h1>}
       </div>
