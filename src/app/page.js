5c54207f2ef54f391aebec84571d47781ed247ca
Francia-bdz <francia.bidzimou@mmibordeaux.com>
2023-11-16 16:35:15 +0100

    add style homepage

--- src/app/page.js
+++ src/app/page.js
@@ -5,13 +5,36 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className={styles.main}>
+      <div className={styles.background}>
+        <img src="/homeGradient.png" className={styles.background_img2} />
+        <img src="/homeVideo.gif" className={styles.background_img} />
+      </div>
       <div className={styles.title}>
-        <h1>DEGROWTH</h1>
-        <h2>2100 .</h2>
+        <div className={styles.logo}>
+          <img src="d_logo.png" alt="D" className={styles.logo_1} />
+          <p className={styles.logo_2}>EGROWTH</p>
+        </div>
+        <div className={`${styles.logo} ${styles.blur_1}`}>
+          <img src="d_logo.svg" alt="D" className={styles.logo_1} />
+          <p className={styles.logo_2}>EGROWTH</p>
+        </div>
+        <div className={`${styles.logo} ${styles.blur_2}`}>
+          <img src="d_logo.svg" alt="D" className={styles.logo_1} />
+          <p className={styles.logo_2}>EGROWTH</p>
+        </div>
+        <div className={`${styles.logo} ${styles.blur_3}`}>
+          <img src="d_logo.svg" alt="D" className={styles.logo_1} />
+          <p className={styles.logo_2}>EGROWTH</p>
+        </div>
+        <div className={`${styles.logo} ${styles.blur_4}`}>
+          <img src="d_logo.svg" alt="D" className={styles.logo_1} />
+          <p className={styles.logo_2}>EGROWTH</p>
+        </div>
       </div>
       <Link className={styles.start} href="/story1">
         {" "}
-        Démarrer l'histoire
+        Démarrer
+        <img src="arrow.svg" alt="arrow" className={styles.arrow} />
       </Link>
     </main>
   );
